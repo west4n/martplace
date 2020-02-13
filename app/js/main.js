f3d2c3651022eae72bd26912a397bbae223da275
Kursky Ivan <iskurskiii@gmail.com>
2020-02-14 00:01:42 +0300

    added search filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,13 @@
 $(function(){
 
+    $('.search__inner-default').click(function(){
+        $(this).parent().toggleClass("active");
+    })
+
+    $('.search__inner-select li').click(function(){
+        let current = $(this).html();
+        $('.search__inner-default li').html(current);
+        $('.search__inner').removeClass('active');
+    })
 
 });
