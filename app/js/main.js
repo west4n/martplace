b6607e7a404f5c3e8553ae098b45649aa8194ffe
Kursky Ivan <iskurskiii@gmail.com>
2020-03-05 01:31:37 +0300

    added terms page, added header and footer responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,32 @@
 $(function(){
     var mixer = mixitup('.newest-filter__wrapper');
     
+    $('.header__burger').click(function(){
+        $('.header__burger,.menu').toggleClass('active')
+    });
+
+    $('.dropdown-home').click(function(){
+        $('.dropdown__menu-home').toggleClass('active')
+    });
+    $('.dropdown-products').click(function(){
+        $('.dropdown__menu-products').toggleClass('active')
+    });
+    $('.dropdown-wordpress').click(function(){
+        $('.dropdown__menu-wordpress').toggleClass('active')
+    });
+    $('.dropdown__pages').click(function(){
+        $('.dropdown__wrapper').toggleClass('active')
+    });
+    $(document).on("click", function(e) {
+        if ($(e.target).hasClass('header__dropdown-btn'))
+        $(".header__info").toggleClass("active");
+        else
+        $(".header__info").removeClass("active");
+    });
+    $('.header__dropdown-close').click(function(){
+        $('.header__info').removeClass('active')
+    });
+
 
     $('.search__inner-default').click(function(){
         $(this).parent().toggleClass("active");
